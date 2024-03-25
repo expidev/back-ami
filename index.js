873d2485f88da1a18ed1942cf1286a628afaec8a
Assistant-info-users <example@example.com>
2024-03-25 16:00:27 +0300

    almost done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const route = require("./routes/route");
-const cors = require("cors")
+const authRoute = require("./routes/authRoute");
+const passport = require('passport');
+const session = require('express-session')
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
@@ -9,11 +12,17 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(session({ 
+    secret: 'secret', 
+    resave: false, 
+    saveUninitialized: false
+ }));
 
-app.get('/', (req, res) => {
-    res.send("Hello");
-})
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfiguration");
 
+app.use('/', authRoute);
 app.use('/', route);
 
 app.listen(port, () => {
--- routes/route.js
+++ routes/route.js
@@ -1,14 +1,17 @@
 const documentsController = require("../controllers/documentController");
-
-const visitorModel = require("../controllers/visitorController");
+const visitorController = require("../controllers/visitorController");
+const amiController = require("../controllers/amiController");
+const { ensureAuthenticated } = require("../middleware/authMiddleware");
 
 const router = require("express").Router();
 
-router.post('/ajout', documentsController.uploadAndInsert);
+router.post('/ajout', ensureAuthenticated,documentsController.uploadAndInsert);
 
-router.post('/', visitorModel.insert)
+router.post('/', visitorController.insert)
 
-router.get('/visiteur/:id_visiteur', visitorModel.getVisitorById)
+router.get('/ami', amiController.getList);
+
+router.get('/visiteur/:id_visiteur', visitorController.getVisitorById)
 
 router.get('/documents/:id_ami', documentsController.getListByAmi)
 
