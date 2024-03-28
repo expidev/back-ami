698beaa73bb5bc1c98d6f32c10c7d9b818898666
Assistant-info-users <example@example.com>
2024-03-28 14:33:23 +0300

    learned nodemailer

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,7 @@ const documentsController = require("../controllers/documentController");
 const visitorController = require("../controllers/visitorController");
 const amiController = require("../controllers/amiController");
 const adminController = require("../controllers/adminController");
+const tokenController = require("../controllers/tokenController");
 const { authenticateAdmin } = require("../middleware/authMiddleware");
 
 const router = require("express").Router();
@@ -10,7 +11,7 @@ router.post('/signin', adminController.post);
 
 router.post('/ajout', authenticateAdmin, documentsController.uploadAndInsert);
 
-router.post('/', visitorController.insert)
+router.post('/', tokenController.requestDownload, visitorController.insert)
 
 router.get('/ami', authenticateAdmin, amiController.getList);
 
