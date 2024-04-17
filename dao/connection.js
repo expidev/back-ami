594f8c551ec37ee3389b90f7947331d4055dc5d5
Assistant-info-users <example@example.com>
2024-04-17 14:51:52 +0300

    done

--- dao/connection.js
+++ dao/connection.js
@@ -8,7 +8,7 @@ let pool;
 if (process.env.NODE_ENV === 'development') {
     // For development environment
     pool = mysql.createPool({
-        connectionLimit: 10,
+        connectionLimit: 20,
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
