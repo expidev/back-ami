f5c13a327fb9e1c5a574d9066491c82973f1f833
Assistant-info-users <example@example.com>
2024-04-17 15:38:48 +0300

    done

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -4,7 +4,7 @@ const amiModel = require("../dao/amiModel")
 const { removeFile, downloadFile, downloadZipDocuments } = require('../helper');
 const path = require('path');
 
-// Use a dynamic insertId for each request
+// change the destination and the filename of the file upload
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/dao_ami');
@@ -30,13 +30,10 @@ const getListByAmi = async (req, res) => {
 
 const uploadAndInsert = async (req, res) => {
     try {
-        // Call the upload middleware to handle file upload
         upload(req, res, async function (err) {
             if (err instanceof multer.MulterError) {
-                // A Multer error occurred when uploading.
                 return res.status(400).json({ message: "File upload error" });
             } else if (err) {
-                // An unknown error occurred when uploading.
                 return res.status(500).json({ message: "Internal Server Error" });
             }
 
